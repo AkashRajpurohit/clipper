5ba69052be8f5f2585cc99c1ed072fc3904a3451
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-02-26 17:42:03 +0530

    :art: react setup for client side

--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const Clipper = () => {
+    return (
+        <h1>Clipper! 📋</h1>
+    );
+}
+
+ReactDOM.render(
+    <Clipper />,
+    document.getElementById('root')
+)
