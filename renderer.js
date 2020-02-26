1940fa092bb65b47f2c6cc891ecbfad2202a7c51
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2020-02-26 22:50:22 +0530

    :art: when clicked, move the item to top

--- renderer.js
+++ renderer.js
@@ -1,78 +1,97 @@
 class Clipper extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            history: [],
-            interval: -1
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [],
+      interval: -1
     }
+  }
 
-    loadHistory = () => {
-        // Get all history from localstorage
-        const historyFromLocalstorage = JSON.parse(window.localStorage.getItem('clipper')) || []
+  getNewItemId = (array) => {
+    if (array.length === 0) return 1
 
-        return historyFromLocalstorage
-    }
+    return array[0].id + 1
+  }
 
-    addHistoryToLocalstorage = (text) => {
-        let historyFromLocalstorage = JSON.parse(window.localStorage.getItem('clipper')) || []
+  loadHistory = () => {
+    // Get all history from localstorage
+    const historyFromLocalstorage = JSON.parse(window.localStorage.getItem('clipper')) || []
 
-        // Add new text to localstorage
-        historyFromLocalstorage = [text, ...historyFromLocalstorage]
+    return historyFromLocalstorage
+  }
 
-        // Save updated history to localstorage
-        window.localStorage.setItem('clipper', JSON.stringify(historyFromLocalstorage))
-    }
+  updateLocalstorage = () => {
+    // Clear the exisitng histories
+    window.localStorage.removeItem('clipper')
+    // Update with new history data
+    window.localStorage.setItem('clipper', JSON.stringify(this.state.history))
+  }
 
-    textClicked = (e) => {
-        window.copyToClipboard(e.currentTarget.dataset.text)
-    }
+  textClicked = (e) => {
+    const { text } = e.currentTarget.dataset
 
-    componentWillMount() {
-        // Get All previously added histories
-        this.state.history = this.loadHistory()
+    let remainingHistory = this.state.history.filter(item => item.text !== text)
 
-        // Start listening for new texts
-        this.state.interval = setInterval(() => {
-            const text = window.checkClipboard()
+    this.setState({
+      history: [
+        ...remainingHistory
+      ]
+    })
 
-            // Check is first text is same as new text or not
-            if(this.state.history[0] !== text) {
-                // Add this text to history
-                this.setState({ history: [ text, ...this.state.history ]})
-                this.addHistoryToLocalstorage(text)
+    window.copyToClipboard(e.currentTarget.dataset.text)
+  }
 
-                console.log(this.state.history)
-            }
-        }, 1000)
-    }
+  componentWillMount() {
+    // Get All previously added histories
+    this.state.history = this.loadHistory()
 
-    componentWillUnmount() {
-        clearInterval(this.state.interval)
-    }
+    // Start listening for new texts
+    this.state.interval = setInterval(() => {
+      const text = window.checkClipboard()
 
-    render() {
-        return (
-            <div>
-                <h1>Clipper! 📋</h1>
-                <p>{this.state.history.length}</p>
-                <ul>
-                    {
-                        this.state.history.map(text => {
-                            return (
-                                <li data-text={text} key={text} onClick={this.textClicked}>
-                                    {text}
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        );
-    }
+      // Check is first text is same as new text or not
+      if (this.state.history.length === 0 || this.state.history[0].text !== text) {
+        // Add this text to history
+        this.setState({
+          history: [{
+            id: this.getNewItemId(this.state.history),
+            text
+          },
+          ...this.state.history]
+        })
+
+        this.updateLocalstorage()
+      }
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    // Clear the interval
+    clearInterval(this.state.interval)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Clipper! 📋</h1>
+        <p>{this.state.history.length}</p>
+        <ul>
+          {
+            this.state.history.map(({ id, text }) => {
+              return (
+                <li data-text={text} key={id} onClick={this.textClicked}>
+                  {text}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <Clipper />,
-    document.getElementById('root')
+  <Clipper />,
+  document.getElementById('root')
 )
